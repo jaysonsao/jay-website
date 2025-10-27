beb9d60f9726ebc2f0abe65fee828a27ac206b06
Jayson Sao <90787288+jaysonsao@users.noreply.github.com>
2025-10-27 10:55:31 -0400

    Fix build configuration issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./content/**/*.{md,mdx}"
-],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/content/**/*.{md,mdx}",
+  ],
   theme: {
     extend: {},
   },
+  corePlugins: {
+    preflight: false,
+  },
   plugins: [],
-}
+};
 
